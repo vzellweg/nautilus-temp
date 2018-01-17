883e210a4b37469754245d31929bd8c6a90d3ece
Victor Zellweger <vazellweg@gmail.com>
2018-01-17 12:16:32 -0800

    Modify color palette and background color to contrast better and look more cohesive. Change button styling to make text more legible  and remove rounded border corners.

--- postcss.config.js
+++ postcss.config.js
@@ -6,15 +6,14 @@ module.exports = (config) => [
         customProperties: {
           variables: {
             maxWidth: "60rem",
-            colorPrimaryDark: "#87b464",
-            colorPrimary: "#68d476",
+            colorPrimaryDark: "#4b830d",
+            colorPrimary: "#7cb342",
             colorSecondaryDark: "#e9c834",
             colorSecondary: "#eedc4b",
             colorNeutralDark: "#111",
             colorNeutral: "#8C8D91",
             colorNeutralLight: "#FBFCFC",
             colorText: "#555",
-            colorAccent: "#f47b53",
           },
         },
       },
